f53bc537e6885fe977da8f848396882e1e4fd011
emily747 <emily747@sigint.mx>
2025-09-23 21:27:44 -0400

    fix zsh issue

--- main/static/setup.sh
+++ main/static/setup.sh
@@ -4,7 +4,7 @@ yes | sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --d
 # Sets up zsh config
 cat << EOF > ~/.zshrc
 autoload -U colors && colors
-PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+PS1="%{\$fg[red]%}%n%{\$reset_color%}@%{\$fg[blue]%}%m %{\$fg[yellow]%}%~ %{\$reset_color%}%% "
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 EOF
