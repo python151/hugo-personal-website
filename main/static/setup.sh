9d89f2ea04ccd778c3a624d8e4d203a50a49d8c3
emily747 <emily747@sigint.mx>
2025-09-23 21:14:05 -0400

    Added setup script to static

---
+++ main/static/setup.sh
@@ -0,0 +1,20 @@
+# Installs the Nix package manager
+yes | sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+# Updates shell to have nix commands
+exec bash
+
+# Installs packages with nix
+nix-env -iA nixpkgs.zsh nixpkgs.neovim
+
+# Sets up zsh config
+cat << EOF > ~/.zshrc
+autoload -U colors && colors
+PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+EOF
+
+# Sets up zsh as the default shell
+chsh -s $(which zsh)
+
